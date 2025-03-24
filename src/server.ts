efd0ecfc8660d0769148d872441e77e7e560b5e7
Amir-m-Arabi <arabiamir2005@gmail.com>
2025-03-24 19:42:08 +0330

    updating cors function

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ import swaggerOutput from '../swagger-output.json'
 
 const app = express()
 
-const allowedOrigins = ['http://localhost:3000', 'https://tl-front-eight.vercel.app'];
+const allowedOrigins = ['http://localhost:3000', 'https://tl-front-eight.vercel.app/'];
 
 app.use(cors({
     origin: allowedOrigins,
