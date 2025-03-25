ab4456db557136cef90db21619f90ab5ff1e1e89
Amir-m-Arabi <arabiamir2005@gmail.com>
2025-03-25 19:41:13 +0330

    update name of rout in task.ts file in routes directory

--- src/routes/task.ts
+++ src/routes/task.ts
@@ -6,6 +6,6 @@ export default (router : express.Router) => {
     router.post('/create-task', tokenValidationMiddleware , Title , createTask)
     router.get('/get-tasks', tokenValidationMiddleware , getTasks)
     router.put('/update-task/:id', tokenValidationMiddleware , Title , updateTask)
-    router.put("update-status/:id" , tokenValidationMiddleware , updateStatus)
+    router.put("/update-status/:id" , tokenValidationMiddleware , updateStatus)
     router.delete('/delete-task/:id', tokenValidationMiddleware , deleteTask)
 }
